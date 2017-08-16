e08d12374c455e6a68fad34c4a2a51dd74ce9278
ArkhamEng <ArkhamEng@ARKHAMENGHPPRO>
2017-08-16 16:45:55 -0500

    Add project files.

---
+++ CerberusMultiBranch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CerberusMultiBranch.MvcApplication" Language="C#" %>
---
+++ CerberusMultiBranch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CerberusMultiBranch.Startup))]
+namespace CerberusMultiBranch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
